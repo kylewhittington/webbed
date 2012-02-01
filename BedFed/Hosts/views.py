0616f0c54409918c69200aaf85be9762cb1d8caa
Kyle Whittington <flipflop@hopeful.(none)>
2012-02-01 20:26:05 +0000

    basic search func and jquery integration

--- BedFed/Hosts/views.py
+++ BedFed/Hosts/views.py
@@ -5,8 +5,20 @@ import Hosts
 from Hosts.models import Property
 
 def search(request):
-    propertylist = Property.objects.all()
-    return render_to_response('search.html', locals())
-    
+    #propertylist = Property.objects.all()
+    error = False
+    if 'search_terms' in request.GET and 'arrival_date' in request.GET and 'dept_date' in request.GET:
+        search_terms = request.GET['search_terms']
+        arrival_date = request.GET['arrival_date']
+        dept_date = request.GET['dept_date']
+        if not search_terms or not arrival_date or not dept_date:
+            error = True
+        else:
+            #books = Book.objects.filter(title__icontains=q)
+            properties = Property.objects.filter(city__icontains=search_terms)
+            return render_to_response('search_results.html',
+                    {'properties': properties, 'search_terms': search_terms })
+    return render_to_response('home.html',{'error': error})
+
 def home(request):
     return render_to_response('home.html')
