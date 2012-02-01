f16841a3c59dbb612b0bf4629bcdae96c3e8330f
Kyle Whittington <flipflop@hero>
2012-02-01 17:47:04 +0000

    model renaming

--- BedFed/Hosts/models.py
+++ BedFed/Hosts/models.py
@@ -10,13 +10,13 @@ from django.contrib.auth.models import User
 
 class Property(models.Model):
     user = models.ForeignKey(User)
-    addressline1 = models.CharField(max_length=200)
-    addressline2 = models.CharField(max_length=200)
+    addressline1 = models.CharField('address line 1',max_length=200)
+    addressline2 = models.CharField('address line 2',max_length=200,blank=True)
     city = models.CharField(max_length=50)
     postcode = models.CharField(max_length=7)
-    short_desc = models.CharField(max_length=400)
-    long_desc = models.TextField(max_length=5000)
-    heading = models.CharField(max_length=50)
+    short_desc = models.TextField('short description',max_length=400)
+    long_desc = models.TextField('long description',max_length=5000)
+    heading = models.CharField('search heading',max_length=50)
     #availability = models.ForeignKey(Availability)
     def __unicode__(self):
         return u'%s - %s' % (self.user.first_name, self.addressline1)
@@ -32,7 +32,7 @@ class Review(models.Model):
     user = models.ForeignKey(User)
     date = models.DateTimeField('date reviewed')
     rating = models.IntegerField(choices=RATING.items(),default=3)
-    text = models.TextField(max_length=5000)
+    text = models.TextField(max_length=5000,blank=True)
 
 class Booking(models.Model):
     user = models.ForeignKey(User)
